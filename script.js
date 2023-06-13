8813f6c0f06b174cfbc5a10686b47a2ed59061e9
Wiolly <wiolly@hotmail.com>
2023-06-13 12:44:59 +0100

    functions updated, classes changed

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-const input = document.querySelector(".input");
+const nameInput = document.querySelector(".user-name");
+const avatarInput = document.querySelector(".user-avatar");
 const button = document.querySelector(".button");
 const textarea = document.querySelector(".textarea");
 
-button.addEventListener("click", send);
-
-function send() {
-  const userName = document.createElement("userName");
-  userName.innerText = input.value;
-  const nameContainer = document.querySelector("#your-name");
-  nameContainer.appendChild(userName);
+button.addEventListener("click", checkSpam);
+function checkSpam(str) {
+  return str.replace(/viagra|xxx/gi, "***");
 }
 
 button.addEventListener("click", user);
 function user() {
-  input.value.trim();
-  input.value.charAt(0).toUpperCase + input.value.slice(1).toLowerCase;
-  input.value = " ";
+  nameInput.value.trim();
+  nameInput.value.charAt(0).toUpperCase + nameInput.value.slice(1).toLowerCase;
 }
 
-button.addEventListener("click", avatar);
-function avatar() {
-  const img = document.createElement("img");
-  img.src = "path/to/image.jpg";
+button.addEventListener("click", send);
+function send() {
+  const userName = document.createElement("userName");
+  userName.innerText = nameInput.value;
+  const nameContainer = document.querySelector("#your-name");
+  nameContainer.appendChild(userName);
+
+  const userAvatar = document.createElement("userAvatar");
+  userAvatar.src = avatarInput.value;
   const container = document.querySelector("#image-container");
-  container.append(img);
-}
+  container.appendChild(userAvatar);
 
-button.addEventListener("click", addChat);
-function addChat() {
   const comment = document.createElement("comment");
   comment.innerText = textarea.value;
   const commentContainer = document.querySelector("#chat");
   commentContainer.appendChild(comment);
-}
-
-button.addEventListener("click", checkSpam);
-
-function checkSpam(str) {
-  const spam = textarea.replace(/viagra/gi, "***");
-  const spam2 = textarea.replace(/XXX/gi, "***");
-  return spam || spam2;
 }
