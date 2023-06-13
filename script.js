6c9283d4cdb3d88942983b1b9f60f06da8d5cb7e
Wiolly <wiolly@hotmail.com>
2023-06-13 22:44:09 +0100

    functions updates

--- script.js
+++ script.js
@@ -3,21 +3,19 @@ const avatarInput = document.querySelector(".user-avatar");
 const button = document.querySelector(".button");
 const textarea = document.querySelector(".textarea");
 
-button.addEventListener("click", checkSpam);
 function checkSpam(str) {
   return str.replace(/viagra|xxx/gi, "***");
 }
 
-button.addEventListener("click", user);
 function user() {
-  nameInput.value.trim();
-  nameInput.value.charAt(0).toUpperCase + nameInput.value.slice(1).toLowerCase;
+  const userName = nameInput.value.trim();
+  return userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase();
 }
 
 button.addEventListener("click", send);
 function send() {
   const userName = document.createElement("userName");
-  userName.innerText = nameInput.value;
+  userName.innerText = user(nameInput.value);
   const nameContainer = document.querySelector("#your-name");
   nameContainer.appendChild(userName);
 
@@ -27,7 +25,7 @@ function send() {
   container.appendChild(userAvatar);
 
   const comment = document.createElement("comment");
-  comment.innerText = textarea.value;
+  comment.innerText = checkSpam(textarea.value);
   const commentContainer = document.querySelector("#chat");
   commentContainer.appendChild(comment);
 }
